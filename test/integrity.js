765478d0deb7e3872dcd76bf3edbffbdcff2a469
Li, Bruce <BeiBei.Li@carsguide.com.au>
2016-02-25 16:29:38 +1100

    make jsdoc possible

--- test/integrity.js
+++ test/integrity.js
@@ -1,24 +1,24 @@
 // integrity test
 var assert = require('assert');
-var App = require('../src/app.js').SimpleApp;
+var SimpleApp = require('../dist/simple.dev.js').SimpleApp;
 
 describe('Simple App', function () {
   describe('constructor', function () {
     it('should have a default config', function () {
       var name = 'test-app';
-      var app = new App(name);
-      assert.ok(app.name == name, 'Name should be the same');
-      assert.ok(Object.keys(app.config).length > 0, 'default config exists');
+      var app = SimpleApp(name);
+      assert.ok(app.aName == name, 'Name should be the same');
+      assert.ok(Object.keys(app.cnf).length > 0, 'default config exists');
     });
     it('Should make each app unique', function () {
-      var app1 = new App('App1', {
-        shouldSaveStateToLocalStorage: false
+      var app1 = SimpleApp('App1', {
+        localStorageWrite: false
       });
-      var app2 = new App('App2', {
-        shouldSaveStateToLocalStorage: true
+      var app2 = SimpleApp('App2', {
+        localStorageWrite: true
       });
       assert.notEqual(app1.name, app2.name);
-      assert.notEqual(app1.config, app2.config);
+      assert.notEqual(app1.cnf, app2.cnf);
       assert.notEqual(app1, app2);
     });
   });
