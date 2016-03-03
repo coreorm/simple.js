157fb6d049f1e81c4ff173e1f6ebdd3ddbb03a44
Li, Bruce <BeiBei.Li@carsguide.com.au>
2016-03-03 15:22:30 +1100

    unit test done

--- test/spec.js
+++ test/spec.js
@@ -6,6 +6,8 @@ describe("Localstorage Test", function () {
   });
 
   before(function () {
+    // clear local storage
+    localStorage.clear();
   });
 
   it("State operation: localStorageWrite: false", function () {
@@ -69,55 +71,74 @@ describe('Uniqueness of apps', function () {
 
 });
 
-/* test for app: render (no data update, initial) */
 describe('Render', function () {
-  it('Wrapper Elements & Normal Elements ', function () {
-    var app = SimpleApp('render-test');
-    app.template.main.default = '<div>{foo}{bar}</div>';
-    app.template.sub = {
-      foo: {
-        default: '<div {attr}>{_txt}</div>'
+  /* test for app: render (no data update, initial) */
+  var app = SimpleApp('render-test');
+  app.template.main.default = '<div>{foo}{bar}</div>';
+  app.template.sub = {
+    foo: {
+      default: '<div {attr}>{_txt}</div>'
+    },
+    bar: {
+      _wrapper: ['<ul {attr}>', '</ul>'],
+      default: '<li {attr}>{_lbl}</li>'
+    }
+  };
+
+  app.data.foo = {
+    _txt: 'this is foo'
+  };
+
+  app.data.bar = {
+    wrapper: {},
+    element: [
+      {
+        class: 'list',
+        _lbl: 'item 1'
       },
-      bar: {
-        _wrapper: ['<ul {attr}>', '</ul>'],
-        default: '<li {attr}>{_lbl}</li>'
+      {
+        class: 'list',
+        _lbl: 'item 2'
+      },
+      {
+        class: 'list',
+        _lbl: 'item 3'
       }
-    };
+    ]
+  };
 
-    app.data.foo = {
-      _txt: 'this is foo'
-    };
-
-    app.data.bar = {
-      wrapper: {},
-      element: [
-        {
-          class: 'list',
-          _lbl: 'item 1'
-        },
-        {
-          class: 'list',
-          _lbl: 'item 2'
-        },
-        {
-          class: 'list',
-          _lbl: 'item 3'
-        }
-      ]
-    };
-
+  it('Wrapper Elements & Normal Elements ', function () {
     app.init(document.getElementById('main_app'), true);
     expect(document.getElementsByClassName('list').length).to.equal(3);
+  });
+
+  it('should render additional items without touching existing ones', function () {
     // test additional rendering without breaking existing.
     document.getElementsByClassName('list')[0].innerHTML = 'updated';
     app.data.bar.element.push({
-      class:'list',
+      class: 'list',
       _lbl: 'item 4'
     });
     app.render();
-    console.log(document.getElementById('main_app').innerHTML);
+    //console.log(document.getElementById('main_app').innerHTML);
     expect(document.getElementsByClassName('list').length).to.equal(4);
     expect(document.getElementsByClassName('list')[0].innerHTML).to.equal('updated');
+  });
+
+  it('should remove nodes without touching existing ones', function () {
+    // test reducing data (remove items)
+    app.data.bar.element.splice(3, 1);
+    app.data.bar.element.splice(2, 1);
+    document.getElementsByClassName('list')[0].innerHTML = 'updated #2';
+    app.render();
+    expect(document.getElementsByClassName('list').length).to.equal(2);
+    expect(document.getElementsByClassName('list')[0].innerHTML).to.equal('updated #2');
+  });
+
+  it('should force render all', function () {
+    app.render(true);
+    expect(document.getElementsByClassName('list').length).to.equal(2);
+    expect(document.getElementsByClassName('list')[0].innerHTML).to.not.equal('updated #2');
   });
 
 });
