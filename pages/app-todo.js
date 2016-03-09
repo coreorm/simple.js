5f4d6ae69dcc0e3cb8aecc5057b7dae8740ef844
Bruce <toughgamer@gmail.com>
2016-03-09 23:18:46 +1100

    add src

--- pages/app-todo.js
+++ pages/app-todo.js
@@ -4,12 +4,19 @@
  */
 (function () {
   // make it available
-  document.getElementById('apps').innerHTML = '<h1>To-do List <small>standalone edition</small> ' +
-    src('apps/example/todo.js') + '</h1>' +
-    '<p>Click the src button on the top right corner to view the source of this app. For more examples, ' +
+  document.getElementById('apps').innerHTML = '<h1>To-do List <small>standalone edition</small></h1>' +
+    '<p>The simple to-do list app. Scroll down to the source section and see for yourself how simple the entire app is.</p><p>For more examples, ' +
     'see <a href="#example">examples</a> page. </p>' +
-    '<div class="panel panel-primary"><div class="panel-body" id="todo"></div></div>';
+    '<div class="panel panel-primary"><div class="panel-body" id="todo"></div></div>' +
+    '<h3>Source:</h3><div><pre><code id="src"></code></pre></div>';
   // load related apps
   loadJs('apps/example/todo.js');
+  // load code
+  jQuery.get('apps/example/todo.js').done(function (src) {
+    document.getElementById('src').innerHTML = src.replace(/</g, '&lt;');
+    jQuery('pre code').each(function (i, block) {
+      hljs.highlightBlock(block);
+    });
+  });
 
 })();
